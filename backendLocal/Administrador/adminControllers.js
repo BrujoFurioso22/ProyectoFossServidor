1d8d2f053a02ddbbc84d513c0475c488c80374c6
Diego Barbecho <diegobarbecho133@gmail.com>
2024-03-13 22:39:58 -0500

    Actualizacion 13-03-2024

--- backendLocal/Administrador/adminControllers.js
+++
@@ -1,4 +0,0 @@
-import { ConsultarImagenesAdmin ,EstadoImagenAdmin, SubirImagen} from "./adminService.js";
-
-export { ConsultarImagenesAdmin, EstadoImagenAdmin, SubirImagen };
-// Agregar más controladores según sea necesario
